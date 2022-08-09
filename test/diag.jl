72d49bfcc6d8ce27bf56747523e46468f80d15dd
Bartek Gardas <bartek.gardas@gmail.com>
2022-08-09 14:02:14 +0200

    clean up tests

--- test/diag.jl
+++ test/diag.jl
@@ -1,4 +1,3 @@
-
 using Snake
 using LinearAlgebra
 
@@ -8,7 +7,10 @@ num_states = 1
 @testset "Correct results when no interaction" begin
     for T ∈ (Float16, Float32, Float64)
         h = diagm(rand(T, L))
+
         sp = exhaustive_search(h, num_states=num_states)
+
+        @test eltype(sp.energies[1]) == T
         @test sp.energies[1] ≈ -sum(h)
         @test vec(sp.states) == fill(-1, L)
     end
